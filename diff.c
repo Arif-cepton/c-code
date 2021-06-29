b3f50fa8746394d53743a25ce2de182330fcb69e
ARIF SAYYAD <arif.sayyad@cepton.com>
2021-06-29 15:56:11 +0530

    adding diff file

---
+++ diff.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int num1=15,num2=10,sub;
+    
+  
+   
+    //formula of subtraction
+    printf("Differences of two numbers are: %d\n",sub);
+ 
+    return 0;
+}
